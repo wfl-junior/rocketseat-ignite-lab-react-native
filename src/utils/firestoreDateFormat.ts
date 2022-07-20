4e9fb36d00bef86f9532ed22405ab2956c253026
Wallace Júnior <wflj1997@hotmail.com>
2022-07-20 00:20:09 -0300

    Aprimorar util firestoreDateFormat

--- src/utils/firestoreDateFormat.ts
+++ src/utils/firestoreDateFormat.ts
@@ -9,8 +9,10 @@ export function firestoreDateFormat(
     date = timestamp.toDate();
   }
 
-  const day = date.toLocaleDateString("pt-BR");
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear().toString().padStart(4, "0");
   const hour = date.toLocaleTimeString("pt-BR");
 
-  return `${day} às ${hour}`;
+  return `${day}/${month}/${year} às ${hour}`;
 }
