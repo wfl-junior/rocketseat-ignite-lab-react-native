5595ffa40a1e522770d4e67a2dc178a41edc3043
Wallace Júnior <wflj1997@hotmail.com>
2022-07-20 22:18:55 -0300

    Melhorar estado de erro dos inputs

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,4 +1,5 @@
-import { IInputProps, Text, VStack } from "native-base";
+import { HStack, IInputProps, Text, useTheme, VStack } from "native-base";
+import { WarningCircle } from "phosphor-react-native";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
 import { Input } from "./Input";
 
@@ -28,56 +29,62 @@ export const FormInput = <T extends FieldValues = FieldValues>({
   control,
   error,
   ...props
-}: InputProps<T>): JSX.Element => (
-  <VStack
-    w="full"
-    m={m}
-    mt={mt}
-    mr={mr}
-    mb={mb}
-    ml={ml}
-    margin={margin}
-    marginTop={marginTop}
-    marginRight={marginRight}
-    marginBottom={marginBottom}
-    marginLeft={marginLeft}
-    mx={mx}
-    my={my}
-    marginX={marginX}
-    marginY={marginY}
-    flex={flex}
-  >
-    <Controller
-      name={name}
-      control={control}
-      render={({ field: { onChange, value } }) => (
-        <Input
-          bg="gray.700"
-          h={14}
-          size="md"
-          borderWidth={1}
-          borderColor="transparent"
-          fontSize="md"
-          fontFamily="body"
-          color="white"
-          placeholderTextColor="gray.300"
-          _focus={{
-            borderWidth: 1,
-            borderColor: "green.500",
-            bg: "gray.700",
-          }}
-          flex={flex}
-          {...props}
-          value={value}
-          onChangeText={onChange}
-        />
-      )}
-    />
+}: InputProps<T>): JSX.Element => {
+  const { colors } = useTheme();
 
-    {!!error && (
-      <Text color="red.500" mt={1}>
-        {error}
-      </Text>
-    )}
-  </VStack>
-);
+  return (
+    <VStack
+      w="full"
+      m={m}
+      mt={mt}
+      mr={mr}
+      mb={mb}
+      ml={ml}
+      margin={margin}
+      marginTop={marginTop}
+      marginRight={marginRight}
+      marginBottom={marginBottom}
+      marginLeft={marginLeft}
+      mx={mx}
+      my={my}
+      marginX={marginX}
+      marginY={marginY}
+      flex={flex}
+    >
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            bg="gray.700"
+            h={14}
+            size="md"
+            borderWidth={1}
+            borderColor={error ? "red.500" : "transparent"}
+            fontSize="md"
+            fontFamily="body"
+            color="white"
+            placeholderTextColor="gray.300"
+            _focus={{
+              borderWidth: 1,
+              borderColor: "green.500",
+              bg: "gray.700",
+            }}
+            flex={flex}
+            {...props}
+            value={value}
+            onChangeText={onChange}
+          />
+        )}
+      />
+
+      {!!error && (
+        <HStack mt={1} alignItems="center" space={2}>
+          <WarningCircle size={14} color={colors.red[500]} />
+
+          <Text color="red.500">{error}</Text>
+        </HStack>
+      )}
+    </VStack>
+  );
+};
